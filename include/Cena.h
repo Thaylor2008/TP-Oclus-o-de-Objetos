e96508d9ab26134e5b023aeea2ed81c5346c7e85
Thaylor2008 <137810643+Thaylor2008@users.noreply.github.com>
2025-10-08 20:43:53 -0300

    comentarios

--- include/Cena.h
+++ include/Cena.h
@@ -1,9 +1,5 @@
-// include/Cena.h
 #ifndef CENA_H
 #define CENA_H
-
-#include <iostream>
-#include <iomanip>
 
 struct TrechoVisivel {
     int id;
@@ -16,14 +12,19 @@ private:
     
     int tempo;
     int n;        // número de objetos visíveis
-    static const int cap_max = 100;
-    TrechoVisivel visiveis[cap_max];
+    static const int cap_max = 100; // capacidade max de objetos visiveis na cena
+    TrechoVisivel visiveis[cap_max]; // vetor com os objetos visiveis em cena 
+    
 
 public:
     Cena(int tempo = 0, int capacidade = 100); // construtor
 
     int getTempo() const;
+    
+    // Adiciona objeto visivel 
     void adicionarVisivel(int id, double inicio, double fim);
+    
+    // Imprimi os objetos visiveis na cena
     void imprimir() const;
     
 };
--- src/Cena.cpp
+++ src/Cena.cpp
@@ -1,13 +1,11 @@
-// src/Cena.cpp
 #include "Cena.h"
 #include <iomanip>
-#include <cstdlib>
+#include <iostream>
 
-
-Cena::Cena(int tempo, int capacidade)
+Cena::Cena(int tempo, int capacidade) // construtor
     : tempo(tempo), n(0) {}
 
-
+// Adiciona objeto visivel 
 void Cena::adicionarVisivel(int id, double inicio, double fim) {
     if (n < cap_max) {
         visiveis[n].id = id;
@@ -20,6 +18,7 @@ int Cena::getTempo() const {
     return tempo;
 }
 
+// Imprimi os objetos visiveis na cena
 void Cena::imprimir() const {
     std::cout << std::fixed << std::setprecision(2);
 
